3dcf0ec197db251af9616bd4230f59e22aab3da2
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2024-06-10 23:50:21 +0500

    Updated Todo App
    new repository update in portfolio

--- scripts.js
+++ scripts.js
@@ -1,44 +1,242 @@
 var projects = [
-  {title: 'ABC Book', img: './projects/abc book.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: "APPLE", img: "./projects/apple.jpg", githubLink: "https://github.com/AbdullahBhatti1987/apple", hostedLink: "https://abdullahbhatti1987.github.io/apple/"},
-  {title: "Attendance Portal", img: "./projects/attendance.jpg", githubLink: "https://github.com/AbdullahBhatti1987/AttendancePortal", hostedLink: "https://abdullahbhatti1987.github.io/AttendancePortal/"},
-  {title: 'CALCULATOR', img: "./projects/calculator.jpg", githubLink: "https://github.com/AbdullahBhatti1987/calculatorapp", hostedLink: "https://abdullahbhatti1987.github.io/calculatorapp/", date: ''},
-  {title: 'color Website', img: "./projects/colorwebsite.jpg", githubLink: "https://github.com/AbdullahBhatti1987/Palette-color", hostedLink: "https://abdullahbhatti1987.github.io/Palette-color/", date: ''},
-  {title: 'Digital Watch', img: "./projects/digital watch.jpg", githubLink: "https://github.com/AbdullahBhatti1987/digital-watch", hostedLink: "https://abdullahbhatti1987.github.io/digital-watch/", date: ''},
-  {title: 'eid Ul Adha ', img: "./projects/eid ul adha.jpg", githubLink: '', hostedLink: '', date: ''},
-  {title: 'Even registration', img: "./projects/even registration.jpg", githubLink: '', hostedLink: '', date: ''},
-  {title: 'Even n Odd', img: "./projects/evenodd.jpg", githubLink: '', hostedLink: '', date: ''},
-  {title: 'Facebook Login ', img: './projects/facebook loginpage.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/Facebook-login-page', hostedLink: 'https://abdullahbhatti1987.github.io/Facebook-login-page/', date: ''},
-  {title: 'Flex Froggy', img: './projects/flexfroggy.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/flexboxfroggy', hostedLink: '', date: ''},
-  {title: 'Grid System', img: './projects/grid system.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/Gridsystem', hostedLink: 'https://abdullahbhatti1987.github.io/Gridsystem/', date: ''},
-  {title: 'Income Expense App', img: './projects/incomeexpense.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/incomeandexpence', hostedLink: 'https://abdullahbhatti1987.github.io/incomeandexpence/', date: ''},
-  {title: 'JS Basics', img: './projects/javascript basic1.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'Light On/Off', img: './projects/light onoff.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/Lightonoff', hostedLink: 'https://abdullahbhatti1987.github.io/Lightonoff/', date: ''},
-  {title: 'Luck Game', img: './projects/luckgame.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'Ludo', img: './projects/ludo.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'Ministore', img: './projects/ministore.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/MiniStore', hostedLink: 'https://abdullahbhatti1987.github.io/MiniStore/', date: ''},
-  {title: 'Navbar', img: './projects/navbar.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'Nice-01', img: './projects/nice-1.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/nicepage', hostedLink: 'https://abdullahbhatti1987.github.io/nicepage/', date: ''},
-  {title: 'Nice-02', img: './projects/nice-2.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'Priodic Table', img: './projects/priodic table.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'rozeepk', img: './projects/rozeepk.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'shopingo', img: './projects/shopingo.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'registration form', img: './projects/smit registration form.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'smit portal', img: './projects/smit.jpg', githubLink: '', hostedLink: '', date: ''},
-  {title: 'stop watch', img: './projects/stopwatch.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/stopwatchbyabdullahbhatti', hostedLink: '', date: ''},
-  {title: 'sunzine', img: './projects/sunzine.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/sunzine', hostedLink: '', date: ''},
-  {title: 'text editor', img: './projects/texteditor.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/Text-Editor', hostedLink: 'https://abdullahbhatti1987.github.io/Text-Editor/', date: ''},
-  {title: 'time table', img: './projects/timetable.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/Time-table', hostedLink: 'https://abdullahbhatti1987.github.io/Time-table/', date: ''},
-  {title: 'todo App', img: './projects/todoapp.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/todoapp', hostedLink: 'https://abdullahbhatti1987.github.io/todoapp/', date: ''},
-  {title: 'travel & tour', img: './projects/travel tour.jpg', githubLink: 'https://github.com/AbdullahBhatti1987/TravelTour', hostedLink: '', date: ''},
-  {title: 'upwork clone', img: './projects/upwork clone.jpg', githubLink: '', hostedLink: '', date: ''}
+  {
+    title: "Todo App Version 2.0",
+    img: "./projects/updatedTodoApp.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Todowithsignup",
+    hostedLink: "https://abdullahbhatti1987.github.io/Todowithsignup/",
+    date: "10-06-2024",
+  },
+  {
+    title: "For Each",
+    img: "./projects/foreach.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/foreachloop",
+    hostedLink: "https://abdullahbhatti1987.github.io/foreachloop/",
+    date: "06-06-2024",
+  },
+  {
+    title: "APPLE",
+    img: "./projects/apple.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/apple",
+    hostedLink: "https://abdullahbhatti1987.github.io/apple/",
+  },
+  {
+    title: "Attendance Portal",
+    img: "./projects/attendance.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/AttendancePortal",
+    hostedLink: "https://abdullahbhatti1987.github.io/AttendancePortal/",
+    date: "",
+  },
+  {
+    title: "CALCULATOR",
+    img: "./projects/calculator.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/calculatorapp",
+    hostedLink: "https://abdullahbhatti1987.github.io/calculatorapp/",
+    date: "",
+  },
+  {
+    title: "color Website",
+    img: "./projects/colorwebsite.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Palette-color",
+    hostedLink: "https://abdullahbhatti1987.github.io/Palette-color/",
+    date: "",
+  },
+  {
+    title: "Digital Watch",
+    img: "./projects/digitalwatch.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/digital-watch",
+    hostedLink: "https://abdullahbhatti1987.github.io/digital-watch/",
+    date: "",
+  },
+  {
+    title: "Countdown Timer",
+    img: "./projects/eiduladha.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Countdown-timer",
+    hostedLink: "https://abdullahbhatti1987.github.io/Countdown-timer/",
+    date: "",
+  },
+  {
+    title: "Counter Application",
+    img: "./projects/evenodd.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Counter-Application",
+    hostedLink: "https://abdullahbhatti1987.github.io/Counter-Application/",
+    date: "",
+  },
+  {
+    title: "Facebook Login ",
+    img: "./projects/fbloginpage.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Facebook-login-page",
+    hostedLink: "https://abdullahbhatti1987.github.io/Facebook-login-page/",
+    date: "",
+  },
+  {
+    title: "Flex Froggy",
+    img: "./projects/flexfroggy.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/flexboxfroggy",
+    hostedLink: "https://abdullahbhatti1987.github.io/flexboxfroggy/#a01",
+    date: "",
+  },
+  {
+    title: "Grid System",
+    img: "./projects/gridsystem.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Gridsystem",
+    hostedLink: "https://abdullahbhatti1987.github.io/Gridsystem/",
+    date: "",
+  },
+  {
+    title: "Income Expense App",
+    img: "./projects/incomeexpense.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/incomeandexpence",
+    hostedLink: "https://abdullahbhatti1987.github.io/incomeandexpence/",
+    date: "",
+  },
+  {
+    title: "JS Basics",
+    img: "./projects/chapter1to4.JPG",
+    githubLink: "https://github.com/AbdullahBhatti1987/Chapter-1-to-4",
+    hostedLink: "https://github.com/AbdullahBhatti1987/Chapter-1-to-4",
+    date: "",
+  },
+  {
+    title: "Light Bulb",
+    img: "./projects/lightbulb.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Lightonoff",
+    hostedLink: "https://abdullahbhatti1987.github.io/Lightonoff/",
+    date: "",
+  },
+  {
+    title: "Luck Game",
+    img: "./projects/luckgame.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/LuckGame",
+    hostedLink: "https://abdullahbhatti1987.github.io/LuckGame/",
+    date: "",
+  },
+  {
+    title: "Ludo",
+    img: "./projects/ludo.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/ludogame",
+    hostedLink: "https://abdullahbhatti1987.github.io/ludogame/",
+    date: "",
+  },
+  {
+    title: "Ministore",
+    img: "./projects/ministore.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/MiniStore",
+    hostedLink: "https://abdullahbhatti1987.github.io/MiniStore/",
+    date: "",
+  },
+
+  {
+    title: "Nice-01",
+    img: "./projects/nice-1.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/nicepage",
+    hostedLink: "https://abdullahbhatti1987.github.io/nicepage/",
+    date: "",
+  },
+  {
+    title: "Interior Services",
+    img: "./projects/nice-2.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/interiorservices",
+    hostedLink: "https://abdullahbhatti1987.github.io/interiorservices/",
+    date: "",
+  },
+  {
+    title: "Priodic Table",
+    img: "./projects/Priodictable.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/priodictable",
+    hostedLink: "https://abdullahbhatti1987.github.io/priodictable/",
+    date: "",
+  },
+  {
+    title: "rozeepk",
+    img: "./projects/rozeepk.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/exam",
+    hostedLink: "https://abdullahbhatti1987.github.io/exam/",
+    date: "",
+  },
+  {
+    title: "Shopingo",
+    img: "./projects/shopingo.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/shopingo",
+    hostedLink: "https://abdullahbhatti1987.github.io/smitportal/Shopingo/home.html",
+    date: "",
+  },
+  {
+    title: "registration form",
+    img: "./projects/registrationform.JPG",
+    githubLink: "https://github.com/AbdullahBhatti1987/smitportal",
+    hostedLink: "https://abdullahbhatti1987.github.io/smitportal/smit/smit%20registration.html",
+    date: "",
+  },
+  {
+    title: "smit portal",
+    img: "./projects/smit.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/smitportal",
+    hostedLink: "https://abdullahbhatti1987.github.io/smitportal/",
+    date: "",
+  },
+  {
+    title: "stop watch",
+    img: "./projects/stopwatch.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/stopwatchbyabdullahbhatti",
+    hostedLink: "https://abdullahbhatti1987.github.io/stopwatchbyabdullahbhatti/",
+    date: "",
+  },
+  {
+    title: "sunzine",
+    img: "./projects/sunzine.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/sunzine",
+    hostedLink: "https://abdullahbhatti1987.github.io/sunzine/",
+    date: "",
+  },
+  {
+    title: "text editor",
+    img: "./projects/texteditor.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Text-Editor",
+    hostedLink: "https://abdullahbhatti1987.github.io/Text-Editor/",
+    date: "",
+  },
+  {
+    title: "time table",
+    img: "./projects/timetable.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Time-table",
+    hostedLink: "https://abdullahbhatti1987.github.io/Time-table/",
+    date: "",
+  },
+  {
+    title: "todo App",
+    img: "./projects/todoapp.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/todoapp",
+    hostedLink: "https://abdullahbhatti1987.github.io/todoapp/",
+    date: "",
+  },
+  {
+    title: "travel & tour",
+    img: "./projects/traveltour.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/TravelTour",
+    hostedLink: "https://abdullahbhatti1987.github.io/TravelTour/",
+    date: "",
+  },
+  {
+    title: "upwork clone",
+    img: "./projects/upworkclone.jpg",
+    githubLink: "https://github.com/AbdullahBhatti1987/Upwork-clone-half-",
+    hostedLink: "https://abdullahbhatti1987.github.io/Upwork-clone-half-/",
+    date: "",
+  },
+  {
+    title: "",
+    img: "./projects/amazonclone.JPG",
+    githubLink: "https://github.com/AbdullahBhatti1987/AmazonClone",
+    hostedLink: "https://abdullahbhatti1987.github.io/AmazonClone/",
+    date: "",
+  },
 ];
-
-
+console.log(projects.length)
 var wrapper = document.getElementById("wrapper");
-var container, counter = 0;
+var container,
+  counter = 0;
 
-projects.forEach(function(data, ind) {
+projects.forEach(function (data, ind) {
   if (counter % 4 === 0) {
     container = document.createElement("div");
     container.className = "container";
@@ -46,23 +244,29 @@ projects.forEach(function(data, ind) {
   }
 
   container.innerHTML += `
-  <div class="card" id="${ind + 1}" style="background-image: url(${data.img});" alt="" onclick="tabdeel(this)"> 
+  <div class="card" id="${ind + 1}" style="background-image: url(${
+    data.img
+  });" alt="" onclick="tabdeel(this)"> 
     <div class="top">
-      <p class="topp">${data.date}</p>
+      <p class="topp"></p>
     </div>  
     <div class="row">
       <div class="icon"><span>${ind + 1}</span></div>
       <div class="description">
-        <h4>${data.title || ''}</h4>
+        <h4>${data.title || ""}</h4>
         <p>${ind + 1}</p>
       </div>
       <div class="buttons">
-        <button onclick="event.stopPropagation(); window.open('${data.githubLink}', '_blank')">Github Code</button>
-        <button onclick="event.stopPropagation(); window.open('${data.hostedLink}', '_blank')">Web View</button>
+        <button onclick="event.stopPropagation(); window.open('${
+          data.githubLink
+        }', '_blank')">Github Code</button>
+        <button onclick="event.stopPropagation(); window.open('${
+          data.hostedLink
+        }', '_blank')">Web View</button>
       </div>
     </div>
   </div>`;
-  
+
   counter++;
 });
 
@@ -70,15 +274,11 @@ var card = document.getElementsByClassName("card");
 
 function tabdeel(element) {
   for (var i = 0; i < card.length; i++) {
-    card[i].classList.remove('active');
+    card[i].classList.remove("active");
     card[i].style.width = "220px";
     card[i].style.height = "400px";
   }
-  element.classList.add('active');
+  element.classList.add("active");
   element.style.width = "600px";
-  element.style.height = "400px";  // Adjust height if necessary
+  element.style.height = "400px"; // Adjust height if necessary
 }
-
-
-
-
